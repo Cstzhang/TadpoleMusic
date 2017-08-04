f6850271527f751186e4ccc30ac419f64a4caae8
Cstzhang <835066041@qq.com>
2017-08-04 10:29:22 +0800

    first commit

---
+++ TadpoleMusic/Pods/Target Support Files/Pods-TadpoleMusic/Pods-TadpoleMusic-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TadpoleMusic : NSObject
+@end
+@implementation PodsDummy_Pods_TadpoleMusic
+@end
---
+++ TadpoleMusic/TadpoleMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TadpoleMusic
+//
+//  Created by zhangzb on 2017/8/4.
+//  Copyright © 2017年 zhangzb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
